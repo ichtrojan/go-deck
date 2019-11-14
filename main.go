2982c23d28c7687e388648b18de623bbd89ea94b
ichtrojan <ordrizzy@gmail.com>
2019-11-15 00:27:29 +0100

    typo fix

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 func main() {
 	cards := newDeck()
-	hand, remaingCards := deal(cards, 5)
+	hand, remainingCards := deal(cards, 5)
 
 	hand.print()
-	remaingCards.print()
+	remainingCards.print()
 }
